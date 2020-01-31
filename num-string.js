817ebcbe7c0a0fc793e8e58a5971591c2c8408eb
Lawrence Kim <lawrey7@gmail.com>
2020-01-31 09:53:58 -0800

    Cleaned up the num-string problem

--- num-string.js
+++ num-string.js
@@ -1,6 +1,6 @@
 function stars(num) {
   var star = '';
-  var diff = (((2 * num) - 1) / 2) - 1;
+  var diff = num - 1;
   for (var i = 1; i <= num; i++) {
     for (var j = 1; j <= ((2 * i) - 1); j++) {
       star += '*';
@@ -16,24 +16,3 @@ function stars(num) {
 }
 // Initial *'s, but no spaces
 stars(1);
-
-// function starsSpaceFun(num) {
-//   var starSpace = '';
-//   var j = 1;
-//   for (var i = 1; i <= num; i++) {
-//     if (j === num) {
-//       starSpace = starSpace + '*';
-//     } else {
-//       starSpace = starSpace + ' ';
-//     }
-//   }
-// }
-
-// starsSpaceFun(1);
-
-// function logArr(num) {
-//   var newArr = [];
-//   for (var i = 1; i <= num; i++) {
-//     newArr
-//   }
-// }
