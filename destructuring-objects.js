76e8b647a1684335043875092d7b90e0a6f4ea7d
Lawrence Kim <lawrey7@gmail.com>
2020-02-14 16:22:27 -0800

    Completed Sum Strings problem.

--- destructuring-objects.js
+++ destructuring-objects.js
@@ -1,10 +1,10 @@
 // ES6: Destructuring Objects VI
 // Function parameters and default values
 
-const str = `
-function shirtSize({size="big"}) {
-  return size
-}`
+// const str = `
+// function shirtSize({size="big"}) {
+//   return size
+// }`
 
 // shirtSize() // error: Cannot destructure property 'size' of 'undefined' or 'null'
 //The preceding code produces an error because no object was passed to the function.
@@ -14,6 +14,6 @@ function shirtSize({size="big"}) {
 //   Do not remove the { size = "big" } object in the parameter
 // Do not change the return statement
 // Example
-shirtSize() //➞ "big"
+//shirtSize() //➞ "big"
 // Notes
 // If you get stuck you can find the answer in one of the yellow notes on the MDN docs page.Link is in the Resources Tab
---
+++ sum-strings.js
@@ -0,0 +1,47 @@
+// Find Sum of Dollar Amounts
+// Take two arguments, an array with string values, and a string, total amount.
+// Each string is made up of a $ sign and a number representing a dollar amount.
+// Compare the sum of the dollar amounts in the array to the total amount string.
+// If the sum is greater than the total amount string, return True, else return False.
+
+function getSum(arr, total) {
+  const amounts = [];
+  let counter = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].charAt(0) === '$') {
+      arr[i] = arr[i].slice(1);
+    }
+    amounts.push(arr[i]);
+  }
+  for (let i = 0; i < amounts.length; i++) {
+    if (amounts[i] == Number(amounts[i])) {
+      counter += Number(amounts[i]);
+    }
+  }
+  if (total.charAt(0) === '$') {
+    total = total.slice(1);
+    if (total == Number(total)) {
+      total = Number(total);
+    }
+  }
+  if (counter > total) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Create a new blank array, amounts
+// Create a counter to store a sum
+// Iterate over each string value in the original given list
+  // Check if there is a $ character at each index
+  // Delete the $ character
+  // Add the new string to the Amounts list
+// Take all the values of the list and compare them to a number value
+  // If the number value and string value are the same, add number to counter
+// Delete the $ character from the total amount string
+// Check if number value and string value are the same
+  // If they are, change the total amount to a number
+// Check if the counter is greater than total
+  // If greater, return True
+  // Or else, return False
